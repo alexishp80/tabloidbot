c1ccd2f944f76d46a2721db9fbcf13e985e44ba1
alexishp80 <alexishaley320@gmail.com>
2024-07-12 01:14:00 -0400

    sketched out rest of bot

--- bot.py
+++ bot.py
@@ -12,20 +12,29 @@ GUILD = os.getenv('DISCORD_GUILD')
 
 bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
 
-@bot.command(name='tabloid')
+@bot.command(name='tabloid', help='Tabloids another CNET')
 async def add(ctx):
     mentionsList = ctx.message.mentions
     victims = []
     perp = ctx.message.author
     for mention in mentionsList:
-        victims.append(mention.name)
-        await ctx.send(mention.name)
-    print(victims)
-    print(perp)
-    #response = discord.Message.mentions
-    #await ctx.send(ctx.author)
+        victims.append(mention.display_name)
+    await ctx.send(f"{perp.display_name} has tabloided {" ".join(victims)}")
+
+#@bot.command(name='leaderboard', help='Shows global statistics of the tabloid')
+#queries database and produces a leaderboard
+#consider having different sortings, such as tabloids, tabloided, and k/d
+
+#@bot.command(name='stats', help='Shows your personal statistics')
+#provide stats for the user who called the command
 
 
+### Restricted to leadership roles ###
+#@bot.command(name='amend', help='Fixes a previous tabloid')
+#will take in a id which matches the line in the database, used to fix entries if needed
+
+#@bot.command(name='delete', help='Deletes a previous tabloid')
+#will delete an entry given the id
 
 @bot.event
 async def on_ready():
